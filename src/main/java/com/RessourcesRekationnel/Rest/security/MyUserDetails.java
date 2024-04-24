edaa03f2286e9a3136f789eb1ee7a7273f217cc7
lucas <thouvenot-lucas@outlook.fr>
2024-04-24 15:19:12 +0200

    patch update user

--- src/main/java/com/RessourcesRekationnel/Rest/security/MyUserDetails.java
+++ src/main/java/com/RessourcesRekationnel/Rest/security/MyUserDetails.java
@@ -18,7 +18,7 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(user.isAdmin()
+        return List.of(user.getAdmin()
                 ? new SimpleGrantedAuthority("ROLE_ADMIN")
                 : new SimpleGrantedAuthority("ROLE_USER"));
     }
