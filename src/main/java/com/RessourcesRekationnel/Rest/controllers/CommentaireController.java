cddfebf45c4324671e66daf759820edeaca97dae
Brian <brian.elmeskiri@icloud.com>
2024-04-28 13:26:33 +0200

    Envoie une erreur 500 si bug coté serveur

--- src/main/java/com/RessourcesRekationnel/Rest/controllers/CommentaireController.java
+++ src/main/java/com/RessourcesRekationnel/Rest/controllers/CommentaireController.java
@@ -27,55 +27,69 @@ public class CommentaireController {
     private UserDao userDao;
 
     // Récupérer les commentaires d'une ressource
-
     @GetMapping("/ressources/{id}/commentaires")
     public ResponseEntity<List<Commentaire>> getCommentaires(@PathVariable(name = "id") Integer id) {
-        Ressource ressource = ressourceDao.findById(id).orElse(null);
-        if (ressource != null) {
-            List<Commentaire> commentaires = commentaireDao.findByRessource(ressource);
-            return ResponseEntity.ok(commentaires);
-        } else {
-            return ResponseEntity.notFound().build();
+        try {
+            Ressource ressource = ressourceDao.findById(id).orElse(null);
+            if (ressource != null) {
+                List<Commentaire> commentaires = commentaireDao.findByRessource(ressource);
+                return ResponseEntity.ok(commentaires);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
+    // Récupérer les commentaires d'un utilisateur
     @GetMapping("/users/{id}/commentaires")
-    public ResponseEntity<List<Commentaire>> getUsersCommenaires(@PathVariable(name = "id")Integer id){
-        User user = userDao.findById(id).orElse(null);
-        if(user!=null){
-            List<Commentaire> commentaires = commentaireDao.findByUser(user);
-            return ResponseEntity.ok(commentaires);
-        }else{
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<List<Commentaire>> getUsersCommenaires(@PathVariable(name = "id") Integer id) {
+        try {
+            User user = userDao.findById(id).orElse(null);
+            if (user != null) {
+                List<Commentaire> commentaires = commentaireDao.findByUser(user);
+                return ResponseEntity.ok(commentaires);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
     // Ajouter un commentaire à une ressource
-
     @PostMapping("/ressources/{id}/commentaires")
     public ResponseEntity<Commentaire> addCommentaire(
             @PathVariable(name = "id") Integer id,
             @RequestBody Commentaire commentaire) {
-        Ressource ressource = ressourceDao.findById(id).orElse(null);
-        if (ressource != null) {
-            commentaire.setRessource(ressource);
-            Commentaire savedCommentaire = commentaireDao.save(commentaire);
-            return ResponseEntity.status(HttpStatus.CREATED).body(savedCommentaire);
-        } else {
-            return ResponseEntity.notFound().build();
+        try {
+            Ressource ressource = ressourceDao.findById(id).orElse(null);
+            if (ressource != null) {
+                commentaire.setRessource(ressource);
+                Commentaire savedCommentaire = commentaireDao.save(commentaire);
+                return ResponseEntity.status(HttpStatus.CREATED).body(savedCommentaire);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
     // Supprimer un commentaire
-
     @DeleteMapping("/ressources/{id}/commentaires/{comId}")
-    public ResponseEntity<String> deleteCommentaire(@PathVariable(name = "id") Integer id,@PathVariable(name = "comId")Integer comId) {
-        Optional<Commentaire> commentaireOptional = commentaireDao.findById(comId);
-        if (commentaireOptional.isPresent()) {
-            commentaireDao.deleteById(comId);
-            return ResponseEntity.ok("Commentaire supprimé avec succès");
-        } else {
-            return ResponseEntity.notFound().build();
+    public ResponseEntity<String> deleteCommentaire(@PathVariable(name = "id") Integer id, @PathVariable(name = "comId") Integer comId) {
+        try {
+            Optional<Commentaire> commentaireOptional = commentaireDao.findById(comId);
+            if (commentaireOptional.isPresent()) {
+                commentaireDao.deleteById(comId);
+                return ResponseEntity.ok("Commentaire supprimé avec succès");
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
